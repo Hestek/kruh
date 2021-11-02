02769a1866d12a6e3988ca11ce3553d66788a0ca
Patrik Sigl <Sigl.Patrik@stud.vassboskovice.cz>
2021-11-02 11:31:11 +0100

    DODĚLAT, NENÍ TU NIC

--- kruh/Program.cs
+++ kruh/Program.cs
@@ -4,9 +4,32 @@ namespace kruh
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ValueType vyber;
+            double prumer;
+            double polomer;
+            {
+                Console.Clear();
+                vyber = Menu();
+                if (vyber == ValueType.prumer)
+                {
+                    radius = 
+
+                }
+            }
+
+
+
         }
+        enum ValueType   
+        { 
+        prumer,
+        polomer
+        }
+
+
+        https://github.com/loxly80/Kruh_IT1B/blob/master/Kruh_IT1B/Program.cs
     }
 }
